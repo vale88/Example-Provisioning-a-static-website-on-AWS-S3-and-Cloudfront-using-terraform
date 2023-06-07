24ea4906b9c35fccbea06251be75712cc1e4b8e4
valentino_nsukuzonke <valentino.nsukuzonke@gmail.com>
2023-06-07 22:27:10 +0200

    initial commit

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  type        = string
+  description = "The AWS Region to use"
+  default     = "us-east-1"
+}
+
+variable "bucket_prefix" {
+  type        = string
+  description = "The prefix for the S3 bucket"
+  default     = "tf-s3-website-"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "The domain name to use"
+  default     = "demo.hands-on-awscloud.com"
+}
